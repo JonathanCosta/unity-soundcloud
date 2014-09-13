2923349f1903d517628d3fe516da001b9db4c98e
Raymond Benc <r@phpfox.com>
2014-09-13 18:20:19 +0200

    Updating actions

--- actions.js
+++ actions.js
@@ -19,8 +19,17 @@ soundcloud_post = function(obj) {
 Core.action.soundcloud = function() {
 
 	if ($('.activity_feed_form_share').length && !$('.soundcloud_link').length) {
+
+		// Find SoundCloud menu and add an icon
+		$('.menu_' + APP_ID).find('span.icon').css({
+			border: '0px',
+			background: 'url(\'//da1a3suzxtd3z.cloudfront.net/assets/apps/soundcloud/menu.png\') no-repeat'
+		});
+
+		// Add SoundCloud phrase to the activity feed form
 		$('.activity_feed_form_attach').append('<li class="soundcloud_link"><a href="#" rel="view_more_link"><div>SoundCloud</div></a></li>');
 
+		// Build the form to add SoundCloud URLs
 		var html = '';
 		html += '<div class="table_clear"><span class="js_attach_holder"><input type="text" class="global_link_input" placeholder="http://" name="val[soundcloud_url]"></span>' +
 			'<ul class="table_clear_button"><li><input type="button" value="Share" class="button" onclick="soundcloud_post(this)" /></li><li class="table_clear_ajax"></li></ul><div class="clear"></div>' +
@@ -28,6 +37,7 @@ Core.action.soundcloud = function() {
 		$('.activity_feed_form_holder').append('<div id="soundcloud_form" class="global_attachment_holder_section" style="display:none;">' + html + '</div>');
 	}
 
+	// Click event to show the SoundCloud form we built earlier
 	$('.soundcloud_link a').click(function() {
 
 		$('.activity_feed_form_attach a.active').removeClass('active');
@@ -40,6 +50,8 @@ Core.action.soundcloud = function() {
 	});
 
 	if ($('#page_core_index_member').length) {
+
+		// Loop thru only our apps feed
 		$('.stream_type_' + APP_ID + ' .activity_feed_json:not(.is_data_build)').each(function() {
 			var this_obj = $(this).parents('.row_feed_loop:first');
 
@@ -47,6 +59,8 @@ Core.action.soundcloud = function() {
 			$(this).addClass('is_data_build');
 			if (html.substr(0, 1) == '{') {
 				html = $.parseJSON(html);
+
+				// Make sure its a SoundCloud object we added
 				if (isset(html.soundcloud)) {
 					var feed_status = this_obj.find('.activity_feed_content_status');
 
@@ -59,6 +73,7 @@ Core.action.soundcloud = function() {
 				}
 			}
 
+			// Scroll even to only load the song if its in view
 			$(window).scroll(function() {
 				if ($Core.isInView(this_obj.find('.activity_feed_content_status')) && !$(this_obj).hasClass('is_build')) {
 					$(this_obj).addClass('is_build');
